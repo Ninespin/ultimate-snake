806752203878f3c117ff22accd718ecb0ed4d883
ArnaudPare-Vogt <arnaud.parevogt@gmail.com>
2016-01-11 10:02:44 -0500

    added basic renderer

---
+++ Ultimate-snake/Ultimate-snake/src/graphics/Renderer.cpp
@@ -0,0 +1,11 @@
+#include "Renderer.h"
+#include <stdio.h>
+
+namespace graphics {
+	unsigned long getFileLength(istream& in)
+	{
+
+
+		return 23;
+	}
+}
---
+++ Ultimate-snake/Ultimate-snake/src/graphics/Renderer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <GL\glew.h>
+#include "Window.h"
+#include "../math/Vec2.h"
+
+
+namespace graphics {
+	
+	//used to render stuff on the screen
+	class Renderer
+	{
+	private:
+	public:
+		int loadShader();
+	};
+
+
+}
