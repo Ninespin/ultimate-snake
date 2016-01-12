60c9a5e55c4e0c4b12a53b063ab50a67bffcd093
Ninespin <guitprodje@hotmail.ca>
2016-01-11 20:44:56 -0500

    +windowConstants.h *fixed snake move *fixed snake die *fixed snake draw to draw and move using windowConstants' wt and ht /changed snake render

--- Ultimate-snake/Ultimate-snake/src/objects/Snake.cpp
+++ Ultimate-snake/Ultimate-snake/src/objects/Snake.cpp
@@ -1,24 +1,28 @@
 #include "Snake.h"
 
 Snake::Snake() {
-	bodypart* p1 = { new bodypart(1,1) };
+	bodypart* p1 = { new bodypart(1,1,&isdead) };
 	addPart(p1);
 	delete p1;
 
-	p1 = { new bodypart(1,2) };
+	p1 = { new bodypart(1,2,&isdead) };
 	addPart(p1);
 	delete p1;
 
-	p1 = { new bodypart(1,3) };
+	p1 = { new bodypart(1,3,&isdead) };
 	addPart(p1);
 	delete p1;
 
-	p1 = { new bodypart(1,4) };
+	p1 = { new bodypart(1,4,&isdead) };
+	addPart(p1);
+	delete p1;
+	p1 = { new bodypart(1,5,&isdead) };
 	addPart(p1);
 	delete p1;
 
 	dx = 1, dy = 0;
-	
+	queued_dx = dx;
+	queued_dy = dy;
 
 }
 Snake::~Snake() {
@@ -26,24 +30,30 @@ Snake::~Snake() {
 }
 
 void Snake::move() {
-	for (std::vector<bodypart>::iterator i = parts.begin(); i != parts.end(); i++) {
-		(*i).cycle();
-		if (i == parts.begin()) {
-			(*i).x += dx;
-			(*i).y += dy;
-		}else{
-			(*i).x = (*(i - 1)).lastX;
-			(*i).y = (*(i - 1)).lastY;
+	if (!isdead) {
+		dx = queued_dx;
+		dy = queued_dy;
+		for (std::vector<bodypart>::iterator i = parts.begin(); i != parts.end(); i++) {
+			(*i).cycle();
+			if (i == parts.begin()) {
+				(*i).x += dx;
+				(*i).y += dy;
+			}
+			else {
+				(*i).x = (*(i - 1)).lastX;
+				(*i).y = (*(i - 1)).lastY;
+			}
+
 		}
+		checkIfDead();
 
 	}
-	checkIfDead();
-
+	
 }
 
 void Snake::checkIfDead() {
 	//is out of the screen
-	if ( parts.at(0).x >= 9 || parts.at(0).x <= 0 || parts.at(0).y >= 9 || parts.at(0).y <= 0) {//note: 9 is screen wt, change that for static var of window eventually
+	if ( parts.at(0).x >= NB_SQUARES_WIDTH || parts.at(0).x < 0 || parts.at(0).y >= NB_SQUARES_HEIGHT || parts.at(0).y < 0) {
 		die();
 		return;
 
@@ -61,37 +71,37 @@ void Snake::checkIfDead() {
 }
 
 void Snake::die(){
-	dx = 0;
-	dy = 0;
-	for (std::vector<bodypart>::iterator i = parts.begin(); i != parts.end(); i++) {
+	dx = dy = queued_dx = queued_dy = 0;
+	isdead = true;
+	/*for (std::vector<bodypart>::iterator i = parts.begin(); i != parts.end(); i++) {
 		(*i).color.r = 255;
 		(*i).color.g = 0;
 		(*i).color.b = 255;
 
-	}
+	}*/
 }
 
 void Snake::updateKeyboard(const Keyboard& key)
 {
 	if (key.isKeyJustPressed(GLFW_KEY_UP) && dy == 0)
 	{
-		dy = 1;
-		dx = 0;
+		queued_dy = 1;
+		queued_dx = 0;
 	}
 	else if (key.isKeyJustPressed(GLFW_KEY_DOWN) && dy == 0)
 	{
-		dy = -1;
-		dx = 0;
+		queued_dy = -1;
+		queued_dx = 0;
 	}
 	else if (key.isKeyJustPressed(GLFW_KEY_RIGHT) && dx == 0)
 	{
-		dy = 0;
-		dx = 1;
+		queued_dy = 0;
+		queued_dx = 1;
 	}
 	else if (key.isKeyJustPressed(GLFW_KEY_LEFT) && dx == 0)
 	{
-		dy = 0;
-		dx = -1;
+		queued_dy = 0;
+		queued_dx = -1;
 	}
 }
 	
@@ -135,6 +145,10 @@ int Snake::getDy() {
 	return dy;
 }
 
+bool Snake::isDead() {
+	return isdead;
+}
+
 std::vector<bodypart> Snake::getBodyparts() {
 	return parts;
 }
@@ -158,17 +172,22 @@ void Snake::setDy(int y) {
 
 
 
-bodypart::bodypart(int x1, int y1){
+bodypart::bodypart(int x1, int y1,bool* b){
 	x = x1;
 	y = y1;
+	parentIsDead = b;
 }
 
 void bodypart::draw() {
 	glBegin(GL_TRIANGLE_FAN);
 
-	glColor3f(color.r, color.g, color.b);
-		glVertex2f(x+ width / 2, y+ height / 2);//center of the box
+	if(*parentIsDead)
+		glColor3f(0, 0, 0);
+	else 
+		glColor3f(color.r, color.g, color.b);
 
+		glVertex2f(x+ width / 2, y+ height / 2);//center of the box
+	glColor3f(color.r, color.g, color.b);
 		glVertex2f(x, y);
 		glVertex2f(x + width, y);
 		glVertex2f(x + width, y + height);
